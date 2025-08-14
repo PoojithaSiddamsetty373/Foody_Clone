8dfc78775569ee61eec6988f9f5016e1608acfa1
PoojithaSiddamsetty373 <siddamsettypoojitha@gmail.com>
2025-08-14 15:40:38 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ This is the correct import
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-    
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
